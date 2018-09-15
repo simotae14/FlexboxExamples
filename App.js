b74f7ca56012e5ba0cc31abe3166267dfa08b05b
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-15 20:28:48 +0200

    justifyContent: 'center' example

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ class FlexboxExamples extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-start'
+    justifyContent: 'center'
   },
   box: {
     height: 50,
