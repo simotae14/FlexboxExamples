b2b69ad59fd0e005bb4242170a73504f9552662e
Simone Taeggi <simone.taeggi@leovegas.com>
2018-09-15 21:07:17 +0200

    alignItems: 'flex-start' example

--- App.js
+++ App.js
@@ -16,8 +16,7 @@ class FlexboxExamples extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    alignItems: 'flex-start'
   },
   box: {
     height: 50,
